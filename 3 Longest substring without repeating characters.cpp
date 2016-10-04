1a6e62f2250fa9a269b0d9df8ef353b6cc34f71f
Shane GU <gushixin56@gmail.com>
2016-10-04 17:43:37 -0400

    set using, fantastic

--- 3 Longest substring without repeating characters.cpp
+++ 3 Longest substring without repeating characters.cpp
@@ -1,17 +1,16 @@
-class solution{
+class Solution {
 public:
-	int length(string s){
-		set<char>t;
-		int res = 0, left = 0, right = 0;
-		while(right < s,size()){
-			if(t.find(s[right]) == t.end()){
-				t.insert(s[right++]);
-				res = max(res, (int)t.size());
-			}
-			else {
-				t.erase(s[left++]);
-			}
-		}
-		return res;
-	}
+    int lengthOfLongestSubstring(string s) {
+        set<char> t;
+        int res = 0, left = 0, right = 0;
+        while(right < s.size()){
+            if(t.find(s[right]) == t.end()){
+                t.insert(s[right++]);
+                res = max(res, (int)t.size());
+            } else{
+                t.erase(s[left++]);
+            }
+        }
+        return res;
+    }
 };
